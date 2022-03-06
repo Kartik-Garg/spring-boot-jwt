b1adc2697b044022a4db0feb2776d04b6cb0c6a8
unknown <kartikgarg938@gmail.com>
2022-03-07 00:28:29 +0530

    adding webSecurity class

---
+++ src/main/java/com/example/jwtapi/conig/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.jwtapi.conig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+//This class extends the WebSecurityConfigurerAdapter that enables 
+//both WebSecurity and HTTPSecurity to be customized.
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    
+    @Autowired
+    private UserDetailsService jwtUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        // configure AuthenticationManager so that it knows from where to load
+		// user for matching credentials
+		// Use BCryptPasswordEncoder
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(PasswordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder PasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf().disable()
+        //dont authenticate this particular request, i.e no jwt for this particular request
+        .authorizeRequests().antMatchers("/authenticate").permitAll()
+        //all other requests to be authenticated
+        .anyRequest().authenticated().and()
+        //we use stateless session, because session wont be used to store user's state
+        .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //add a filter to validate the tokens with every request
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
