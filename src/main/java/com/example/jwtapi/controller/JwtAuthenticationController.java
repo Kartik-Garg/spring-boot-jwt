41a3f75ff83dd9f6e75932947cdd837bfd4c3db4
unknown <kartikgarg938@gmail.com>
2022-03-07 03:38:24 +0530

    lots of renaming of variables and project is completed

--- src/main/java/com/example/jwtapi/controller/JwtAuthenticationController.java
+++ src/main/java/com/example/jwtapi/controller/JwtAuthenticationController.java
@@ -42,7 +42,7 @@ public class JwtAuthenticationController {
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
 		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-
+      
 		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
 
 		final String token = jwtTokenUtil.generateToken(userDetails);
